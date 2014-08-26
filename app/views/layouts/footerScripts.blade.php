955b427efb9a58d501bc0f5e56149572693bd54a
Yuriy <yuriy@moscreative.com>
2014-08-25 21:27:54 -0400

    Revision 1.3.3
    -fixed Print Selected button to toggle enabled or disabled

--- app/views/layouts/footerScripts.blade.php
+++ app/views/layouts/footerScripts.blade.php
@@ -60,10 +60,10 @@
         num_rows = $('#inlineEditDataTable tr.row_selected').length;
         if(num_rows > 0){
           $('#email').removeClass('disabled');
-          console.log(num_rows);
+          $('#print').removeClass('disabled');
         }else{
           $('#email').addClass('disabled');
-          console.log(num_rows);
+          $('#print').addClass('disabled');
         }
 
         // FadeIn/Out delete rows button
@@ -74,51 +74,6 @@
         }
       });
 
-
-      // $("#select_all").click(function(){
-      //   $("#inlineEditDataTable tbody tr").addClass('row_selected'); 
-      //   $('#select_all').text('Deselect all');
-      // });
-
-      $("#select_button").click(function(){
-          if ($(this).text() == 'Deselect All'){
-            
-            $(this).text('Select All');
-            $("#inlineEditDataTable tbody tr").removeClass('row_selected');
-
-            
-          }else{
-            $("#inlineEditDataTable tbody tr").addClass('row_selected');
-            $(this).text('Deselect All');
-          }
-          num_rows = $('#inlineEditDataTable tr.row_selected').length;
-          if(num_rows > 0){
-            $('#email').removeClass('disabled');
-            
-          }else{
-            $('#email').addClass('disabled');
-            
-          }
-
-      });
-
-      
-      var oTable02 = $('#inlineEditDataTable').dataTable({
-        "sDom":
-          "R<'row'<'col-md-6'l><'col-md-6'f>r>"+
-          "t"+
-          "<'row'<'col-md-4 sm-center'i><'col-md-4'><'col-md-4 text-right sm-center'p>>",
-        "oLanguage": {
-          "sSearch": ""
-        },
-        "aoColumnDefs": [
-          { 'bSortable': false, 'aTargets': [ "no-sort" ] }
-        ],
-        "fnInitComplete": function(oSettings, json) { 
-          $('.dataTables_filter input').attr("placeholder", "Search");
-        }
-      });
-      
       $("#email").click(function(){
 
         $('#inlineEditDataTable tr.row_selected').each(function() {
@@ -142,6 +97,49 @@
         $('form.print_all_players_form').submit();
       });
 
+
+      $("#select_button").click(function(){
+          if ($(this).text() == 'Deselect All'){
+            
+            $(this).text('Select All');
+            $("#inlineEditDataTable tbody tr").removeClass('row_selected');
+
+            
+          }else{
+            $("#inlineEditDataTable tbody tr").addClass('row_selected');
+            $(this).text('Deselect All');
+          }
+          num_rows = $('#inlineEditDataTable tr.row_selected').length;
+          if(num_rows > 0){
+            $('#email').removeClass('disabled');
+            $('#print').removeClass('disabled');
+            
+          }else{
+            $('#email').addClass('disabled');
+            $('#print').addClass('disabled');
+            
+          }
+
+      });
+
+      
+      var oTable02 = $('#inlineEditDataTable').dataTable({
+        "sDom":
+          "R<'row'<'col-md-6'l><'col-md-6'f>r>"+
+          "t"+
+          "<'row'<'col-md-4 sm-center'i><'col-md-4'><'col-md-4 text-right sm-center'p>>",
+        "oLanguage": {
+          "sSearch": ""
+        },
+        "aoColumnDefs": [
+          { 'bSortable': false, 'aTargets': [ "no-sort" ] }
+        ],
+        "fnInitComplete": function(oSettings, json) { 
+          $('.dataTables_filter input').attr("placeholder", "Search");
+        }
+      });
+      
+      
 
 
 
