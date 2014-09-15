ab9d46260d985b7b5a1f204a22cd03f34368b36c
pioneer903 <pioneer903@users.noreply.github.com>
2014-09-15 12:03:46 -0400

    Update UserTableSeeder.php

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -1,16 +1,1 @@
-<?php
-class UserTableSeeder extends Seeder {
 
-    public function run()
-    {
-        // !!! All existing users are deleted !!!
-        DB::table('users')->delete();
-
-        User::create(array(
-            'username'        => 'admin',
-            'first_name'      => 'Administrator',
-            'password'        => Hash::make('!Jrbluecamp2'),
-            'email'           => 'admin@localhost'
-        ));
-    }
-}
